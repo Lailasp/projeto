dc3d030cbd8fb2e9093554654180e6b0327bb7b2
Ledro11 <lab9@aluno.ifal.edu.br>
2025-05-27 11:08:56 -0300

    Criação do arquivo agenda e alteração na versão do sdk, pois o computador é uma versão diferente da qual estava usando.

---
+++ lib/agenda.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class agenda extends StatefulWidget {
+  const agenda({super.key});
+
+  @override
+  State<agenda> createState() => _agendaState();
+}
+
+class _agendaState extends State<agenda> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
