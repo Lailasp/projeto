366aaf7ddbc193ffaba7a09446b745ff1ded1f84
zezeca-dev <jens2@aluno.ifal.edu.br>
2025-06-11 16:08:37 -0300

    Mudança da versão do SDK no pubspec.yaml pela diferença de computadores.
    Adição da fontes no pubspec.lock.
    
    Mudanças na agenda.dart
    Adição do título "Agenda"
    Criação do container que contém a agenda.
    No container inseri elementos apenas textuais por enquanto. Como a data.

--- lib/agenda.dart
+++ lib/agenda.dart
@@ -1,4 +1,6 @@
+import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 class agenda extends StatefulWidget {
@@ -11,142 +13,96 @@ class agenda extends StatefulWidget {
 
 
 class _agendaState extends State<agenda> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFF3D9ABA),
-        body: Column(
+        body: buildBody(),
+        bottomNavigationBar: ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          child: SlidingClippedNavBar(
+          backgroundColor: Colors.white,
+          onButtonPressed: (index) {},
+          iconSize: 25,
+          //activeColor: Color(0xFFC3EBF8),
+          activeColor: Color(0xFF3D9ABA),
+          selectedIndex: selectedIndex,
+          barItems: [
+            BarItem(icon: FontAwesomeIcons.house, title: 'Diário'),
+            BarItem(icon: FontAwesomeIcons.solidCalendarDays, title: 'Agenda'),
+            BarItem(icon: FontAwesomeIcons.solidMessage, title: 'Mensagens'),
+            BarItem(icon: FontAwesomeIcons.solidCircleUser, title: 'Perfil'),
+          ],
+        ),
+      ),
+      )
+    );
+  }
+
+  buildBody(){
+    return Padding(
+      padding: EdgeInsets.only(top: 16, bottom: 16),
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              "Mensagens",
-              style: TextStyle(
-                fontSize: 28,
+            Text("Agenda", style: TextStyle(color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.bold )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 720,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
                 color: Colors.white,
-                fontWeight: FontWeight.w600,
               ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(32)),
-                ),
-                margin: EdgeInsets.only(top: 5, bottom: 32, left: 5, right: 5),
-                padding: EdgeInsets.all(5),
+              child: Column(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 70,
+                    padding: EdgeInsets.only(right: 14, left: 14, top: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TextButton(onPressed: () {}, child: Text("Dia", style: TextStyle(color: Color(0xFF3d9aba), fontSize: 20.0),)),
+                        TextButton(onPressed: () {}, child: Text("Semana", style: TextStyle(color: Color(0xFF3d9aba),fontSize: 20.0),)),
+                        TextButton(onPressed: () {}, child: Text("Mês", style: TextStyle(color: Color(0xFF3d9aba),fontSize: 20.0),))
+                      ],
+                    ),
+                  ),
+                  Divider(),
 
+                  Container(
+                  width: 400,
+                  height: 50,
+                  padding: EdgeInsets.only(right: 9, left: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("14", style: TextStyle(color: Color(0xFF3d9aba),)),
+                      Text("ABRIL", style:TextStyle(color: Color(0xFF3d9aba),fontSize: 20.0, fontWeight: FontWeight.bold)),
+                      Text("2025", style: TextStyle(color: Color(0xFF3d9aba),)),
+                    ],
+                  ),
+                  ),
 
-                child: ListView(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        radius: 28,
-                        backgroundImage: NetworkImage(
-                          'https://static.vecteezy.com/ti/vetor-gratis/p1/1886209-doctor-medical-cartoon-design-vetor.jpg',
-                        ),
-                      ),
-                      title: Text(
-                        'Dr. Carlos João',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFF3D9ABA),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'Vamos saber o motivo de seu cansaço essa semana',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFFCACCCC),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Divider(indent: 80, endIndent: 24),
-                    ListTile(
-                      leading: CircleAvatar(
-                        radius: 28,
-                        backgroundImage: NetworkImage(
-                          'https://img.lovepik.com/png/20231112/obgyn-clipart-female-naturopathic-doctor-in-lab-coat-with-cells_572098_wh860.png',
-                        ),
-                      ),
-                      title: Text(
-                        'Dr. Maria Lúcia',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFF3D9ABA),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'Vamos marcar sua próxima avaliação para essa semana.',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFFCACCCC),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Divider(indent: 80, endIndent: 24),
-                    ListTile(
-                      leading: CircleAvatar(
-                        radius: 28,
-                        backgroundImage: NetworkImage(
-                          'https://img.pikbest.com/png-images/qiantu/cartoon-female-doctor-original-elements_2537037.png!w700wp',
-                        ),
-                      ),
-                      title: Text(
-                        'Dr. Beatriz',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFF3D9ABA),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'Analisei seus gráficos e estão ótimos!',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFFCACCCC),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Divider(indent: 80, endIndent: 24),
-                    ListTile(
-                      leading: CircleAvatar(
-                        radius: 28,
-                        backgroundImage: NetworkImage(
-                          'https://images.vexels.com/content/144170/preview/character-doctor-woman-d6ee7a.png',
-                        ),
-                      ),
-                      title: Text(
-                        'Dr. Mariana',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFF3D9ABA),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      subtitle: Text(
-                        'Estou feliz com seu progresso!',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Color(0xFFCACCCC),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    Divider(indent: 80, endIndent: 24),
-                  ],
-                ),
+                ],
               ),
-            ),
+            )
           ],
         ),
       ),
     );
   }
+
+
 }
 
 
