5c4a9a8e797fbfbc9ba58d38ee271f16a719dce4
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-08-19 11:56:58 -0300

    Update propriedade.dart

--- lib/domain/propriedade.dart
+++ lib/domain/propriedade.dart
@@ -8,4 +8,20 @@ class Propriedade {
     required this.nomeDR,
     required this.mensagem,
   });
+
+  factory Propriedade.fromJson(Map<String, dynamic> json) {
+    return Propriedade(
+      urlImage: json['urlImage'],
+      nomeDR: json['nomeDR'],
+      mensagem: json['mensagem'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'urlImage': urlImage,
+      'nomeDR': nomeDR,
+      'mensagem': mensagem,
+    };
+  }
 }
