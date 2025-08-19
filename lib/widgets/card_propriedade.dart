4cbc95c5d140f64bd44fbd56dbb04f276b34f06d
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-08-19 11:53:17 -0300

    Update card_propriedade.dart

--- lib/widgets/card_propriedade.dart
+++ lib/widgets/card_propriedade.dart
@@ -1,6 +1,7 @@
-import 'package:projeto/domain/propriedade.dart';
 import 'package:flutter/material.dart';
+import 'package:projeto/domain/propriedade.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 
 class CardPropriedade extends StatefulWidget {
   Propriedade propriedade;
@@ -25,61 +26,69 @@ class _CardPropriedadeState extends State<CardPropriedade> {
 
   //Scaffold
   //backgroundColor: Color(0xFF3D9ABA),
-    // body: Column(
-    // children: [
-    // Text(
-    // "Mensagens",
-    // style: TextStyle(
-    // fontSize: 30,
-    // color: Colors.white,
-    // fontWeight: FontWeight.w600,
-    // ),
-    // ),
+  // body: Column(
+  // children: [
+  // Text(
+  // "Mensagens",
+  // style: TextStyle(
+  // fontSize: 30,
+  // color: Colors.white,
+  // fontWeight: FontWeight.w600,
+  // ),
+  // ),
 
-  @override
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 10, bottom: 20, left: 7, right: 7),
-      padding: EdgeInsets.all(5),
-      child: ListView(
+      margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 4,
+            offset: Offset(2, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Nome do profissional e avatar
           ListTile(
+            contentPadding: EdgeInsets.zero,
             leading: CircleAvatar(
               radius: 28,
-              backgroundImage: NetworkImage(
-                'https://static.vecteezy.com/ti/vetor-gratis/p1/1886209-doctor-medical-cartoon-design-vetor.jpg',
+              backgroundImage: NetworkImage(propriedade.urlImage),
+            ),
+            title: Text(
+              propriedade.nomeDR,
+              style: GoogleFonts.roboto(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: Text(
+              'Profissional da Saúde',
+              style: GoogleFonts.roboto(
+                fontSize: 14,
+                color: Colors.grey[700],
               ),
             ),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.network(propriedade.urlImage),
-          ),
-          SizedBox(height: 16),
-          Row(
-            children: [
-              buildText(
-                text: propriedade.local,
-                fontSize: 17.0,
-                fontWeight: FontWeight.w600,
-              ),
-              Spacer(),
-              Icon(Icons.star, size: 16),
-              buildText(
-                text: '4.76',
-                fontSize: 16.0,
-                color: Colors.black,
-              ),
-            ],
-          ),
-          buildText(text: widget.propriedade.tipoHost),
-          buildText(text: widget.propriedade.datas),
-          buildText(
-            text: '\$${widget.propriedade.valor} total',
-            fontWeight: FontWeight.w600,
+          const SizedBox(height: 12),
+          // Mensagem
+          Text(
+            propriedade.mensagem,
+            style: GoogleFonts.roboto(
+              fontSize: 16,
+              color: Colors.black87,
+            ),
           ),
         ],
       ),
     );
   }
+}
