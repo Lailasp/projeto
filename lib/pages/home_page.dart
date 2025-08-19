94075d0817881f17ccde503b72ef41441e78391b
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-08-19 11:56:07 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:projeto/db/db_helper.dart';
+import 'package:projeto/pages/explore_page.dart' show ExplorePage;
+import 'package:projeto/pages/mensagens_page.dart' show MensagensPage;
 import 'package:sliding_clipped_nav_bar/sliding_clipped_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:projeto/pages/explore_page.dart';
+
 
 class HomePage2 extends StatefulWidget {
   const HomePage2({super.key});
@@ -11,20 +14,23 @@ class HomePage2 extends StatefulWidget {
 }
 
 class _HomePage2State extends State<HomePage2> {
-  int selectedIndex = 0;
+  int selectedIndex = 2;
 
   var pages = [
     ExplorePage(),
-    Center(child: Text('Pagina 1', style: TextStyle(fontSize: 32))),
     Center(child: Text('Pagina 2', style: TextStyle(fontSize: 32))),
-    Center(child: Text('Pagina 3', style: TextStyle(fontSize: 32))),
+    MensagensPage(),
     Center(child: Text('Pagina 4', style: TextStyle(fontSize: 32))),
   ];
 
   @override
   Widget build(BuildContext context) {
+    // Remover
+    DBHelper().initDB();
+
     return SafeArea(
       child: Scaffold(
+        body: pages[selectedIndex],
         backgroundColor: const Color(0xFF3D9ABA),
         bottomNavigationBar: ClipRRect(
           borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
@@ -35,7 +41,7 @@ class _HomePage2State extends State<HomePage2> {
                 selectedIndex = index;
               });
             },
-            iconSize: 25,
+            iconSize: 30,
             activeColor: const Color(0xFF3D9ABA),
             selectedIndex: selectedIndex,
             barItems: [
@@ -46,7 +52,6 @@ class _HomePage2State extends State<HomePage2> {
             ],
           ),
         ),
-
       ),
     );
   }
