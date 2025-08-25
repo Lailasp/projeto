dc9348ce31501ee4cfab1cfcf21e9e511986199d
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-08-25 18:57:35 -0300

    Update explore_page.dart

--- lib/explore_page.dart
+++ lib/explore_page.dart
@@ -1,45 +1,42 @@
+import 'package:teste1/db/database.dart' show Database;
+import 'package:teste1/widgets/card_propriedade.dart';
 import 'package:flutter/material.dart';
-import 'package:projeto/card_propriedades.dart';
-import 'package:projeto/propriedades_dao.dart';
-
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({super.key});
-
 
   @override
   State<ExplorePage> createState() => _ExplorePageState();
 }
 
-
 class _ExplorePageState extends State<ExplorePage> {
-  List listaPropriedades = [];
-
-
-  @override
-  void initState() {
-    super.initState();
-    loadData();
-  }
-
-
-  loadData() async {
-    listaPropriedades = await PropriedadesDao().listarPropriedades();
-    setState(() {});
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20),
-      child: ListView.builder(
-        itemCount: listaPropriedades.length,
-        itemBuilder: (context, i) {
-          return CardPropriedades(
-            propriedades: listaPropriedades[i],
-          );
-        },
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      child: Column(
+        children: [
+          const Text(
+            'Mensagens',
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 10),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: Database.propriedades.length,
+              itemBuilder: (context, i) {
+                return CardPropriedade(
+                  propriedade: Database.propriedades[i],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
