8d3583697062eb52a26641a3328713975b514697
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-10-28 16:28:09 -0300

    Update and rename Address.dart to quote.dart

---
+++ lib/domain/quote.dart
@@ -0,0 +1,15 @@
+class Quote {
+  final String quoteText;
+  final String quoteAuthor;
+
+  Quote({required this.quoteText, required this.quoteAuthor});
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(
+      quoteText: json['quoteText'] ?? '',
+      quoteAuthor: (json['quoteAuthor'] == null || json['quoteAuthor'].isEmpty)
+          ? 'Unknown'
+          : json['quoteAuthor'],
+    );
+  }
+}
